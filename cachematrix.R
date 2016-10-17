f558e0a432a09aedef5787bf26a0174e1237f345
ybowen2 <yashnabowen@gmail.com>
2016-10-16 17:06:49 -0700

    comments added and done

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Editor: Yashna Bowen
 
-##makeCacheMatrix 
-## Write a short comment describing this function
+## makeCacheMatrix and cacheSolve work together for both 
+## matrix inverse computation and also retrieving the cache
+## copy of the inverse when it has already been computed
+
+
+##--------------------------------------------------------------------
+
+## makeCacheMatrix is based on the fact that R uses lexical scoping in 
+## assignments and retrieval. It returns what is basically an object
+## of type makeCacheMatrix in the OOP sense. This is in fact a list
+## which contains pointers to all the internal functions of the 
+## makeCacheMatrix function.
+
+## This way by using the list we can get, set the inverse of matrix 
+## indirectly through those inner functions in the makeCacheMatrix 
+## environment
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -19,7 +33,14 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+
+##------------------------------------------------------------------
+
+## cacheSolve takes an argument of the type makeCacheMatrix which in
+## this case is the list object returned. The $ is then used to get the 
+## cached inverse if no changes are found in the matrix previously passed in
+## and to compute the inverse for a new matrix. This is done through 
+## accessing the nested functions of makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
 
